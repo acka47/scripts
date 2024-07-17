a1e8c2e02fc3000334713f00065e0eb06cc54724
Fabian Steeg <steeg@hbz-nrw.de>
2024-07-17 13:39:53 +0200

    Add path to rpb-spatial entries as `SKOS.definition` (RPB-146)

--- skos2json/skos2json.py
+++ skos2json/skos2json.py
@@ -11,14 +11,15 @@ qres = g.query(
     """
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
-    SELECT ?label ?concept
+    SELECT ?label ?concept ?definition
        WHERE {
            ?concept a skos:Concept ;
-                skos:prefLabel ?label
+                skos:prefLabel ?label ;
+                skos:definition ?definition
        }""")
 
 # Write results to file per line
 with open("rpb-spatial.ndjson", "a") as output:
     for row in qres:
-        output.write("{\"prefLabel\":\"%s\",\"uri\":\"%s\"}" % row) 
+        output.write("{\"prefLabel\":\"%s\",\"uri\":\"%s\",\"definition\":\"%s\"}" % row) 
         output.write("\n") # add a new line delimiter to start new line
